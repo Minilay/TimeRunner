b06e3fd2e85b4baf3d4f8e43f38a5304ffda569b
Michael Meanxson <mlgexgood@gmail.com>
2021-01-12 21:53:30 +0500

    transformed

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        private Transform _transform;
+        
+        [Header("Options")]
+        [SerializeField] private float speed;
+
+        void Start() => _transform = GetComponent<Transform>();
+
+            void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        }
+    }
+}
